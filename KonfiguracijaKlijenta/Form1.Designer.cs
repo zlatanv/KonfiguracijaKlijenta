65bb3224b6a9355d5c1d171e73b49197fcc5b988
Zlatan Voloder <zlatan.voloder@argon.ba>
2024-09-03 15:44:47 +0200

    Add project files.

---
+++ KonfiguracijaKlijenta/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿
+namespace KonfiguracijaKlijenta
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.powerOptionsSwitch = new Telerik.WinControls.UI.RadToggleSwitch();
+            this.lblPower = new Telerik.WinControls.UI.RadLabel();
+            this.log = new Telerik.WinControls.UI.RadTextBoxControl();
+            this.crystalDarkTheme1 = new Telerik.WinControls.Themes.CrystalDarkTheme();
+            this.radLabel2 = new Telerik.WinControls.UI.RadLabel();
+            this.btnDodajOperatera = new Telerik.WinControls.UI.RadButton();
+            this.btnPokreniUpdate = new Telerik.WinControls.UI.RadButton();
+            this.remoteDesktopSwitch = new Telerik.WinControls.UI.RadToggleSwitch();
+            this.btnKonfiguracijaNotifikacija = new Telerik.WinControls.UI.RadButton();
+            this.btnJezikDatum = new Telerik.WinControls.UI.RadButton();
+            this.btnTring = new Telerik.WinControls.UI.RadButton();
+            this.btnAktivirajNetPlwiz = new Telerik.WinControls.UI.RadButton();
+            ((System.ComponentModel.ISupportInitialize)(this.powerOptionsSwitch)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lblPower)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.log)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnDodajOperatera)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnPokreniUpdate)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.remoteDesktopSwitch)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnKonfiguracijaNotifikacija)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnJezikDatum)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnTring)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnAktivirajNetPlwiz)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // powerOptionsSwitch
+            // 
+            this.powerOptionsSwitch.Location = new System.Drawing.Point(143, 297);
+            this.powerOptionsSwitch.Name = "powerOptionsSwitch";
+            this.powerOptionsSwitch.Size = new System.Drawing.Size(67, 20);
+            this.powerOptionsSwitch.TabIndex = 0;
+            this.powerOptionsSwitch.ThemeName = "CrystalDark";
+            this.powerOptionsSwitch.ValueChanged += new System.EventHandler(this.powerOptionsSwitch_ValueChanged);
+            // 
+            // lblPower
+            // 
+            this.lblPower.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPower.Location = new System.Drawing.Point(12, 297);
+            this.lblPower.Name = "lblPower";
+            this.lblPower.Size = new System.Drawing.Size(125, 20);
+            this.lblPower.TabIndex = 1;
+            this.lblPower.Text = "Power Always ON:";
+            // 
+            // log
+            // 
+            this.log.Dock = System.Windows.Forms.DockStyle.Right;
+            this.log.IsReadOnly = true;
+            this.log.Location = new System.Drawing.Point(553, 0);
+            this.log.Multiline = true;
+            this.log.Name = "log";
+            this.log.Size = new System.Drawing.Size(369, 584);
+            this.log.TabIndex = 2;
+            this.log.ThemeName = "CrystalDark";
+            // 
+            // radLabel2
+            // 
+            this.radLabel2.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radLabel2.Location = new System.Drawing.Point(12, 323);
+            this.radLabel2.Name = "radLabel2";
+            this.radLabel2.Size = new System.Drawing.Size(116, 20);
+            this.radLabel2.TabIndex = 5;
+            this.radLabel2.Text = "Remote Desktop:";
+            // 
+            // btnDodajOperatera
+            // 
+            this.btnDodajOperatera.Location = new System.Drawing.Point(12, 21);
+            this.btnDodajOperatera.Name = "btnDodajOperatera";
+            this.btnDodajOperatera.Size = new System.Drawing.Size(198, 24);
+            this.btnDodajOperatera.TabIndex = 6;
+            this.btnDodajOperatera.Text = "Dodaj Operater Account";
+            this.btnDodajOperatera.ThemeName = "CrystalDark";
+            this.btnDodajOperatera.Click += new System.EventHandler(this.btnDodajOperatera_Click);
+            // 
+            // btnPokreniUpdate
+            // 
+            this.btnPokreniUpdate.Location = new System.Drawing.Point(12, 51);
+            this.btnPokreniUpdate.Name = "btnPokreniUpdate";
+            this.btnPokreniUpdate.Size = new System.Drawing.Size(198, 24);
+            this.btnPokreniUpdate.TabIndex = 7;
+            this.btnPokreniUpdate.Text = "Pokreni Sve Update";
+            this.btnPokreniUpdate.ThemeName = "CrystalDark";
+            this.btnPokreniUpdate.Click += new System.EventHandler(this.btnPokreniUpdate_Click);
+            // 
+            // remoteDesktopSwitch
+            // 
+            this.remoteDesktopSwitch.Location = new System.Drawing.Point(143, 323);
+            this.remoteDesktopSwitch.Name = "remoteDesktopSwitch";
+            this.remoteDesktopSwitch.Size = new System.Drawing.Size(67, 20);
+            this.remoteDesktopSwitch.TabIndex = 8;
+            this.remoteDesktopSwitch.ThemeName = "CrystalDark";
+            this.remoteDesktopSwitch.ValueChanged += new System.EventHandler(this.remoteDesktopSwitch_ValueChanged);
+            // 
+            // btnKonfiguracijaNotifikacija
+            // 
+            this.btnKonfiguracijaNotifikacija.Location = new System.Drawing.Point(12, 81);
+            this.btnKonfiguracijaNotifikacija.Name = "btnKonfiguracijaNotifikacija";
+            this.btnKonfiguracijaNotifikacija.Size = new System.Drawing.Size(198, 24);
+            this.btnKonfiguracijaNotifikacija.TabIndex = 9;
+            this.btnKonfiguracijaNotifikacija.Text = "Konfiguracija Notifikacija";
+            this.btnKonfiguracijaNotifikacija.ThemeName = "CrystalDark";
+            this.btnKonfiguracijaNotifikacija.Click += new System.EventHandler(this.btnKonfiguracijaNotifikacija_Click);
+            // 
+            // btnJezikDatum
+            // 
+            this.btnJezikDatum.Location = new System.Drawing.Point(12, 111);
+            this.btnJezikDatum.Name = "btnJezikDatum";
+            this.btnJezikDatum.Size = new System.Drawing.Size(198, 24);
+            this.btnJezikDatum.TabIndex = 10;
+            this.btnJezikDatum.Text = "Konfiguracija Jezika";
+            this.btnJezikDatum.ThemeName = "CrystalDark";
+            this.btnJezikDatum.Click += new System.EventHandler(this.btnJezikDatum_Click);
+            // 
+            // btnTring
+            // 
+            this.btnTring.Location = new System.Drawing.Point(12, 141);
+            this.btnTring.Name = "btnTring";
+            this.btnTring.Size = new System.Drawing.Size(198, 24);
+            this.btnTring.TabIndex = 11;
+            this.btnTring.Text = "Dodaj Tring Folder";
+            this.btnTring.ThemeName = "CrystalDark";
+            this.btnTring.Click += new System.EventHandler(this.btnTring_Click);
+            // 
+            // btnAktivirajNetPlwiz
+            // 
+            this.btnAktivirajNetPlwiz.Location = new System.Drawing.Point(12, 171);
+            this.btnAktivirajNetPlwiz.Name = "btnAktivirajNetPlwiz";
+            this.btnAktivirajNetPlwiz.Size = new System.Drawing.Size(198, 24);
+            this.btnAktivirajNetPlwiz.TabIndex = 12;
+            this.btnAktivirajNetPlwiz.Text = "Aktiviraj Netplwiz";
+            this.btnAktivirajNetPlwiz.ThemeName = "CrystalDark";
+            this.btnAktivirajNetPlwiz.Click += new System.EventHandler(this.btnAktivirajNetPlwiz_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(922, 584);
+            this.Controls.Add(this.btnAktivirajNetPlwiz);
+            this.Controls.Add(this.btnTring);
+            this.Controls.Add(this.btnJezikDatum);
+            this.Controls.Add(this.btnKonfiguracijaNotifikacija);
+            this.Controls.Add(this.remoteDesktopSwitch);
+            this.Controls.Add(this.btnPokreniUpdate);
+            this.Controls.Add(this.btnDodajOperatera);
+            this.Controls.Add(this.radLabel2);
+            this.Controls.Add(this.log);
+            this.Controls.Add(this.lblPower);
+            this.Controls.Add(this.powerOptionsSwitch);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Konfiguracija";
+            ((System.ComponentModel.ISupportInitialize)(this.powerOptionsSwitch)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lblPower)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.log)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnDodajOperatera)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnPokreniUpdate)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.remoteDesktopSwitch)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnKonfiguracijaNotifikacija)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnJezikDatum)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnTring)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnAktivirajNetPlwiz)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Telerik.WinControls.UI.RadToggleSwitch powerOptionsSwitch;
+        private Telerik.WinControls.UI.RadLabel lblPower;
+        private Telerik.WinControls.UI.RadTextBoxControl log;
+        private Telerik.WinControls.Themes.CrystalDarkTheme crystalDarkTheme1;
+        private Telerik.WinControls.UI.RadLabel radLabel2;
+        private Telerik.WinControls.UI.RadButton btnDodajOperatera;
+        private Telerik.WinControls.UI.RadButton btnPokreniUpdate;
+        private Telerik.WinControls.UI.RadToggleSwitch remoteDesktopSwitch;
+        private Telerik.WinControls.UI.RadButton btnKonfiguracijaNotifikacija;
+        private Telerik.WinControls.UI.RadButton btnJezikDatum;
+        private Telerik.WinControls.UI.RadButton btnTring;
+        private Telerik.WinControls.UI.RadButton btnAktivirajNetPlwiz;
+    }
+}
+
---
+++ KonfiguracijaKlijenta/Form1.cs
@@ -0,0 +1,434 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.DirectoryServices.AccountManagement;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using Microsoft.Win32;
+using Telerik.WinControls.UI;
+using System.DirectoryServices;
+using System.IO;
+
+namespace KonfiguracijaKlijenta
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            loadPodataka();
+            CheckRemoteDesktopStatus();
+        }
+
+        public void loadPodataka()
+        {
+            string userName = "operater";
+
+            using (var context = new PrincipalContext(ContextType.Machine))
+            {
+                UserPrincipal user = UserPrincipal.FindByIdentity(context, userName);
+
+                if (user != null)
+                {
+                    log.Text += $"Postoji User Operater\n";
+                    btnDodajOperatera.Enabled = false;
+                }
+                else
+                {
+                    log.Text += $"NE Postoji User Operater\n";
+                    btnDodajOperatera.Enabled = true;
+                }
+            }
+
+            bool isAlwaysOn = CheckPowerOptionsAlwaysOn();
+
+            if (isAlwaysOn)
+            {
+                //  MessageBox.Show("Power options are set to 'Always On' (Never turn off).", "Power Options Check", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                powerOptionsSwitch.Value = true;
+                log.Text += $"Power options are set to 'Always On'(Never turn off)\n";
+            }
+            else
+            {
+              
+              //  MessageBox.Show("Power options are not set to 'Always On'.", "Power Options Check", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                log.Text += $"Power options are not set to 'Always On'.\n";
+                powerOptionsSwitch.Value = false;
+            }
+
+            
+
+        }
+
+        private bool CheckPowerOptionsAlwaysOn()
+        {
+            return IsPowerOptionSetToNever("monitor-timeout-ac") &&
+                   IsPowerOptionSetToNever("standby-timeout-ac") &&
+                   IsPowerOptionSetToNever("disk-timeout-ac");
+        }
+
+        private bool IsPowerOptionSetToNever(string option)
+        {
+            string result = ExecuteCommand($"powercfg /query SCHEME_CURRENT SUB_VIDEO {option}");
+            return result.Contains("0x0") || result.Contains("(Never)");
+        }
+
+        private string ExecuteCommand(string command)
+        {
+            ProcessStartInfo processInfo = new ProcessStartInfo("cmd.exe", "/c " + command)
+            {
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            using (Process process = new Process { StartInfo = processInfo })
+            {
+                process.Start();
+                string output = process.StandardOutput.ReadToEnd();
+                process.WaitForExit();
+                return output;
+            }
+        }
+        private void SetPowerOptionsToNever()
+        {
+            try
+            {
+                ExecuteCommandPowerOptions("powercfg /change standby-timeout-ac 0");
+                ExecuteCommandPowerOptions("powercfg /change standby-timeout-dc 0");
+                ExecuteCommandPowerOptions("powercfg /change hibernate-timeout-ac 0");
+                ExecuteCommandPowerOptions("powercfg /change hibernate-timeout-dc 0");
+
+                log.Text += "Power options set to never for all scenarios.\n";
+            }
+            catch (Exception ex)
+            {
+                log.Text += $"Error setting power options: {ex.Message}\n";
+            }
+        }
+
+        private void ExecuteCommandPowerOptions(string command)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                FileName = "cmd.exe",
+                Arguments = $"/c {command}",
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            using (Process process = Process.Start(startInfo))
+            {
+                using (System.IO.StreamReader reader = process.StandardOutput)
+                {
+                    string result = reader.ReadToEnd();
+                    log.Text += result + "\n";
+                }
+
+                using (System.IO.StreamReader errorReader = process.StandardError)
+                {
+                    string error = errorReader.ReadToEnd();
+                    if (!string.IsNullOrEmpty(error))
+                    {
+                        log.Text += "Error: " + error + "\n";
+                    }
+                }
+            }
+        }
+        private void OpenNotificationSettings()
+        {
+            try
+            {
+                // Open the Notifications settings page in Windows Settings
+                Process.Start("ms-settings:notifications");
+            }
+            catch (Exception ex)
+            {
+                log.Text += $"Error opening Settings: {ex.Message}\n";
+            }
+        }
+
+       
+
+      
+
+        private void AddLocalUser(string username, string password)
+        {
+            try
+            {
+                using (var context = new PrincipalContext(ContextType.Machine))
+                {
+                    // Check if the user already exists
+                    UserPrincipal user = UserPrincipal.FindByIdentity(context, username);
+                    if (user == null)
+                    {
+                        // Create the user
+                        user = new UserPrincipal(context)
+                        {
+                            SamAccountName = username,
+                            UserPrincipalName = username,
+                            PasswordNeverExpires = true,
+                            Enabled = true
+                        };
+
+                        // Save the user
+                        user.Save();
+
+                        // Set password using DirectoryServices
+                        using (var search = new DirectorySearcher(new DirectoryEntry("WinNT://" + Environment.MachineName)))
+                        {
+                            search.Filter = $"(samAccountName={username})";
+                            search.PropertiesToLoad.Add("UserPassword");
+                            var result = search.FindOne();
+                            if (result != null)
+                            {
+                                var entry = result.GetDirectoryEntry();
+                                entry.Invoke("SetPassword", new object[] { password });
+                                entry.CommitChanges();
+                            }
+                        }
+                        log.Text += $"Account Operater kreiran uspješno!...\n";
+                        //MessageBox.Show($"User '{username}' created successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        log.Text += $"Account Operater već postoji...\n";
+                       // MessageBox.Show($"User '{username}' already exists.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show("Error creating user: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                log.Text += $"Error prilikom kreiranja Account-a Operater: {ex.Message}\n";
+            }
+        }
+
+        private void btnDodajOperatera_Click(object sender, EventArgs e)
+        {
+            string username = "Operater";
+            string password = "Argon123";
+            log.Text += $"Dodajem Operater Account...\n";
+            AddLocalUser(username, password);
+        }
+
+        private void btnPokreniUpdate_Click(object sender, EventArgs e)
+        {
+            StartWindowsUpdates();
+        }
+
+        private void StartWindowsUpdates()
+        {
+            try
+            {
+                Process process = new Process();
+                process.StartInfo.FileName = "cmd.exe";
+                process.StartInfo.Arguments = "/c usoclient.exe StartScan";
+                process.StartInfo.Verb = "runas"; // Runs the command as administrator
+                process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                process.Start();
+                process.WaitForExit();
+
+                //MessageBox.Show("Windows Update process started.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                log.Text += $"Windows Update pokrenut!\n";
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show("Error starting Windows Update: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                log.Text += $"Error starting Windows Update: {ex.Message}\n";
+            }
+        }
+
+        private void CheckRemoteDesktopStatus()
+        {
+            try
+            {
+                const string registryKey = @"SYSTEM\CurrentControlSet\Control\Terminal Server";
+                const string valueName = "fDenyTSConnections";
+
+                using (RegistryKey key = Registry.LocalMachine.OpenSubKey(registryKey, false))
+                {
+                    if (key != null)
+                    {
+                        object value = key.GetValue(valueName);
+                        if (value != null)
+                        {
+                            int denyTSConnections = (int)value;
+                            if (denyTSConnections == 0)
+                            {
+                                log.Text += "Remote Desktop is enabled.\n";
+                            }
+                            else
+                            {
+                                log.Text += "Remote Desktop is disabled.\n";
+                            }
+                        }
+                        else
+                        {
+                            log.Text += "Could not retrieve the status of Remote Desktop.\n";
+                        }
+                    }
+                    else
+                    {
+                        log.Text += "Could not open the registry key.\n";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                log.Text += $"Error checking Remote Desktop status: {ex.Message}\n";
+            }
+        }
+
+        private void remoteDesktopSwitch_ValueChanged(object sender, EventArgs e)
+        {
+            if (remoteDesktopSwitch.Value == true)
+            {
+                remoteDesktopSwitch.OnText = "Uključen";
+                EnableRemoteDesktop();
+            }
+            else
+            {
+                remoteDesktopSwitch.OffText = "Isključen";
+                
+            }
+           
+            
+        }
+
+        private void EnableRemoteDesktop()
+        {
+            try
+            {
+                const string registryKey = @"SYSTEM\CurrentControlSet\Control\Terminal Server";
+                const string valueName = "fDenyTSConnections";
+
+                using (RegistryKey key = Registry.LocalMachine.OpenSubKey(registryKey, true))
+                {
+                    if (key != null)
+                    {
+                        key.SetValue(valueName, 0, RegistryValueKind.DWord);
+                        log.Text += "Remote Desktop has been enabled.\n";
+
+                        // Optionally, ensure the firewall allows Remote Desktop connections
+                        EnableFirewallRule();
+                    }
+                    else
+                    {
+                        log.Text += "Could not open the registry key to enable Remote Desktop.\n";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                log.Text += $"Error enabling Remote Desktop: {ex.Message}\n";
+            }
+        }
+
+        private void EnableFirewallRule()
+        {
+            try
+            {
+                Process process = new Process();
+                process.StartInfo.FileName = "cmd.exe";
+                process.StartInfo.Arguments = "/c netsh advfirewall firewall set rule group=\"remote desktop\" new enable=Yes";
+                process.StartInfo.Verb = "runas"; // Run as administrator
+                process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                process.Start();
+                process.WaitForExit();
+
+                log.Text += "Firewall rule for Remote Desktop has been enabled.\n";
+            }
+            catch (Exception ex)
+            {
+                log.Text += $"Error enabling firewall rule: {ex.Message}\n";
+            }
+        }
+
+        private void pokreniNETPLWIZ()
+        {
+            try
+            {
+                Process process = new Process();
+                process.StartInfo.FileName = "cmd.exe";
+                process.StartInfo.Arguments = "/c reg ADD \"HKLM\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\PasswordLess\\Device\" /v DevicePasswordLessBuildVersion /t REG_DWORD /d 0 /f";
+                process.StartInfo.Verb = "runas"; // Run as administrator
+                process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                process.Start();
+                process.WaitForExit();
+
+                log.Text += "Netplwiz Activated.\n";
+            }
+            catch (Exception ex)
+            {
+                log.Text += $"Error Netplwiz: {ex.Message}\n";
+            }
+        }
+
+        private void btnKonfiguracijaNotifikacija_Click(object sender, EventArgs e)
+        {
+            OpenNotificationSettings();
+        }
+
+        private void powerOptionsSwitch_ValueChanged(object sender, EventArgs e)
+        {
+            if (powerOptionsSwitch.Value == true)
+            {
+                SetPowerOptionsToNever();
+            }
+            else
+            {
+
+            }
+        }
+
+        private void btnJezikDatum_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = "ms-settings:regionlanguage",
+                    UseShellExecute = true
+                });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error opening language settings: {ex.Message}");
+            }
+        }
+
+        private void btnTring_Click(object sender, EventArgs e)
+        {
+            string rootPath = @"C:\tring";
+            string xmlPath = Path.Combine(rootPath, "xml");
+
+            try
+            {
+                // Create the root directory
+                Directory.CreateDirectory(rootPath);
+
+                // Create the subdirectory inside the root directory
+                Directory.CreateDirectory(xmlPath);
+
+                log.Text += $"Directories created successfully:\n{rootPath}\n{xmlPath}\n";
+            }
+            catch (Exception ex)
+            {
+                log.Text += $"Error creating directories: {ex.Message}\n";
+            }
+        }
+
+        private void btnAktivirajNetPlwiz_Click(object sender, EventArgs e)
+        {
+            pokreniNETPLWIZ();
+        }
+    }
+}
